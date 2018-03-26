b8f574382d95f3e739596207a1732fc8ea4327cf
LUIZ ROBERTO DE SOUZA OLIVEIRA <luiz.rsoliveira2@senacsp.edu.br>
2018-03-26 11:14:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/luizrsoliveira2/jsonrecycleviewapp/Activies/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.luizrsoliveira2.jsonrecycleviewapp.Activies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.luizrsoliveira2.jsonrecycleviewapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
